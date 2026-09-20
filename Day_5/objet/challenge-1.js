3ea62c9fca289481c7a75a09f90f70dbef80d1b5
ismailmrakna-dev <mraknaismail448@gmail.com>
2026-09-20 22:31:53 +0000

    Add challenge 1 to Object

---
+++ Day_5/objet/challenge-1.js
@@ -0,0 +1,13 @@
+const apprenant = {
+  prenom: "Sara",
+  nom: "Ouadie",
+  age: 21,
+  ville: "Nador",
+  estActif: true,
+};
+console.log(" -----  Access to Object  Dot Notation -----");
+console.log(apprenant.prenom)
+console.log(apprenant.nom)
+console.log(apprenant.age)
+console.log(apprenant.ville)
+console.log(apprenant.estActif)
