e4bfa6da789f74284ba6089c453a242f8c9bb42f
ismailmrakna-dev <mraknaismail448@gmail.com>
2026-09-20 22:39:11 +0000

    Add Challenge 2 to Object

---
+++ Day_5/objet/challenge-2.js
@@ -0,0 +1,11 @@
+const Etudiant = {
+  prenom: "Hamza",
+  nom: "Ouad",
+  note : [10, 20 ,19, 15 ,14],
+};
+console.log (Etudiant.prenom)
+console.log (Etudiant.nom)
+console.log (Etudiant.note)
+console.log (Etudiant.note[0])
+console.log (Etudiant.note[1])
+console.log (Etudiant.note[2])
