1a2f0a0b9fcd9c1037d90f185e2f1c9129fc145c
ismailmrakna-dev <mraknaismail448@gmail.com>
2026-09-25 15:18:42 +0000

    Add datas to candidats

---
+++ Projet/data/data.js
@@ -0,0 +1,166 @@
+const candidats=[
+      { 
+        cin: "RNI001234", 
+        nom: "Alaoui", 
+        prenom: "Yassine", 
+        partiPolitique: "RNI", 
+        age: 48, 
+        electeurs: [ 
+            "E001", "E002", "E003", "E004", "E005", 
+            "E006", "E007", "E008" 
+        ] 
+    }, 
+ 
+    { 
+        cin: "RNI001235", 
+        nom: "Bennani", 
+        prenom: "Amine", 
+        partiPolitique: "RNI", 
+        age: 42, 
+        electeurs: [ 
+            "E009", "E010", "E011", "E012", "E013" 
+        ] 
+    }, 
+ 
+    { 
+        cin: "PAM002341", 
+        nom: "El Mansouri", 
+        prenom: "Omar", 
+        partiPolitique: "PAM", 
+        age: 51, 
+        electeurs: [ 
+            "E014", "E015", "E016", "E017", "E018", "E019" 
+        ] 
+    }, 
+ 
+    { 
+        cin: "PAM002342", 
+        nom: "Chakir", 
+        prenom: "Nadia", 
+        partiPolitique: "PAM", 
+        age: 39, 
+        electeurs: [ 
+            "E020", "E021", "E022", "E023" 
+        ] 
+    }, 
+ 
+    { 
+        cin: "PI003451", 
+        nom: "Baraka", 
+        prenom: "Karim", 
+        partiPolitique: "PI", 
+        age: 55, 
+        electeurs: [ 
+            "E024", "E025", "E026", "E027", "E028", "E029", "E030" 
+        ] 
+    }, 
+ 
+    { 
+        cin: "PI003452", 
+        nom: "Fassi", 
+        prenom: "Mehdi", 
+        partiPolitique: "PI", 
+        age: 44, 
+        electeurs: [ 
+            "E031", "E032", "E033" 
+        ] 
+    }, 
+ 
+    { 
+        cin: "USFP004561", 
+        nom: "El Idrissi", 
+        prenom: "Samir", 
+        partiPolitique: "USFP", 
+        age: 46, 
+        electeurs: [ 
+            "E034", "E035", "E036", "E037" 
+        ] 
+    }, 
+ 
+    { 
+        cin: "MP005671", 
+        nom: "Ait Lahcen", 
+        prenom: "Rachid", 
+        partiPolitique: "MP", 
+        age: 50, 
+        electeurs: [ 
+            "E038", "E039" 
+        ] 
+    }, 
+ 
+    { 
+        cin: "PPS006781", 
+        nom: "Amrani", 
+        prenom: "Hamza", 
+        partiPolitique: "PPS", 
+        age: 37, 
+        electeurs: [ 
+            "E040", "E041", "E042" 
+        ] 
+    }, 
+ 
+    { 
+        cin: "PJD007891", 
+        nom: "Tazi", 
+        prenom: "Youssef", 
+        partiPolitique: "PJD", 
+        age: 45, 
+        electeurs: [ 
+            "E043" 
+        ] 
+    }, 
+ 
+    { 
+        cin: "UC008901", 
+        nom: "Berrada", 
+        prenom: "Anas", 
+        partiPolitique: "UC", 
+        age: 41, 
+        electeurs: [] 
+    }, 
+ 
+    { 
+        cin: "MDS009012", 
+        nom: "Khalfi", 
+        prenom: "Adil", 
+        partiPolitique: "MDS", 
+        age: 52, 
+        electeurs: [] 
+    }, 
+ 
+    { 
+        cin: "FFD010123", 
+        nom: "Najib", 
+        prenom: "Souad", 
+        partiPolitique: "FFD", 
+        age: 38, 
+        electeurs: [] 
+    }, 
+ 
+    { 
+        cin: "CNI011234", 
+        nom: "El Mansouri", 
+        prenom: "Nabil", 
+        partiPolitique: "CNI", 
+        age: 47, 
+        electeurs: [] 
+    }, 
+ 
+    { 
+        cin: "PSU012345", 
+        nom: "Mounir", 
+        prenom: "Sara", 
+        partiPolitique: "PSU", 
+        age: 35, 
+        electeurs: [] 
+    }, 
+ 
+    { 
+        cin: "IND013456", 
+        nom: "Boushaba", 
+        prenom: "Soufiane", 
+        partiPolitique: "Indépendant", 
+        age: 40, 
+        electeurs: [] 
+    }
+]
