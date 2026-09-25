76440d5f70a179267a298599933f51e8d05a1c4a
ismailmrakna-dev <mraknaismail448@gmail.com>
2026-09-25 15:26:09 +0000

    Add Candidat Functions

---
+++ Projet/functions/candidat.js
@@ -0,0 +1,54 @@
+function modifierCandidat(candidats,cin){
+    let index = searchCandidat(candidats,cin)
+    if(index !== -1){
+        let choix=parseInt(prompt("Entrez Votre choix: "))
+        switch(choix){
+           case 1 : 
+                let partiPolitique =prompt("Entez votre nouveau Partie Politique: ");
+                if(partiPolitique==="" || partiPolitique===" " ||partiPolitique==="  ")
+                    partiPolitique = "Independant"
+                candidats[index].partiPolitique = partiPolitique
+                break
+           case 2: 
+                let age=parseInt(prompt("Entez votre nouveau age: "))
+                candidats[index].age=age
+                break
+           case 3: 
+                let nom=prompt("Entez votre nouveau nom: ")
+                candidats[index].nom=nom
+                break 
+        }
+    }
+    else console.log("Il n'y a pas Un candidat avec CIN:  " + cin)
+}
+
+// Calcul length ARRAY
+function lengthArr(arr){
+    let cmp=0
+    for (const ele of arr){
+        cmp++
+    }
+    return cmp
+}
+
+// Supprimer un candidat d'apre votre CIN
+function SupprimerCandidat(candidats, cin){
+    let index=searchCandidat(candidats,cin)
+    if(index !== -1){
+        for (i=index ; i<lengthArr(candidats); i++){
+            candidats[i]=candidats[i+1]
+        }
+        candidats.length -= 1
+    }
+    else console.log("Il n'y a pas Un candidat avec CIN:  " + cin)
+}
+// Recherche un candidat d'apres leur Nom
+function rechercheCandidat(candidats){
+    let nom=prompt("Nom du Candidat : ")
+    for (const candidat of candidats){
+        if(candidat.nom.toLowerCase() === nom.toLocaleLowerCase()){
+            afficherCandidat(candidat)
+            return 1}
+    }
+    return -1
+}
