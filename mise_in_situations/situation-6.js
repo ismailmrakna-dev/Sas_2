2a7212f7e50a15d624a5c7a4e3a9f1f202922f21
ismailmrakna-dev <mraknaismail448@gmail.com>
2026-09-26 17:30:23 +0000

    add situation 6

---
+++ mise_in_situations/situation-6.js
@@ -0,0 +1,7 @@
+const phrases = [
+  "Le chat dort sur le canape",
+  "Il fait beau aujourd'hui",
+  "JavaScript est un langage de programmation puissant et flexible",
+  "Bonjour",
+  "Les etudiants apprennent la logique de programmation",
+];
